25886bdc78c7bc8212c9f7b8db90afab663c6ed3
Michael Lindsey <mackeral@gmail.com>
2013-09-27 22:54:25 +0000

    re-init vm

--- repository.php
+++ repository.php
@@ -4,7 +4,6 @@ require('/home/mackeral/Web/phpIncludes/config.php');
 $institution = $request['institution'];
 
 $m = new MongoClient();
-/*
 $db = $m->selectDB('repos');
 $collection = new MongoCollection($db, 'authors');
 $institutions = $collection->distinct('institution');
@@ -26,11 +25,7 @@ $page->addContent(HTMLLib::form(null, null, HTMLLib::div(
 ), array('class'=>'form-horizontal', 'role'=>'form')));
 
 
-$page->addScript("
-$('#author').typeahead({name: 'author', prefetch: 'ajax.php?action=personalAuthors&institution=$institution'}).bind('typeahead:selected', function(obj, datum){ location.href = 'personalAuthor.php?q=' + datum.value + 'r=thisRepository'; });
-$('#structure').typeahead({name: 'structure', prefetch: 'ajax.php?action=words' }).bind('typeahead:selected', function(obj, datum){ location.href = 'structure.php?q=' + datum.value + 'r=thisRepository'; });
-");
+$page->addScript("$('#author').typeahead( { name: 'author', prefetch: 'ajax.php?action=personalAuthors&institution=$institution' }).bind('typeahead:selected', function(obj, datum){ location.href = 'personalAuthor.php?q=' + datum.value + '&r=$institution'; });
+$('#structure').typeahead({name: 'structure', prefetch: 'ajax.php?action=words' }).bind('typeahead:selected', function(obj, datum){ location.href = 'structure.php?q=' + datum.value + 'r=thisRepository'; });");
 echo $page;
-*/
-echo $institution;
 ?>
